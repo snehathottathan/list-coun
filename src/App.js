bd878d80f3bb59ef56a48e531a0aad2a285daba3
snehathottathan <93931315+snehathottathan@users.noreply.github.com>
2025-08-31 12:15:22 +0530

    cm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../src/Redux/store";
 import LoginPage from "./pages/LoginPage";
@@ -7,14 +7,14 @@ import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-     <Provider store={store}>
-      <Router basename="/list-coun">
+    <Provider store={store}>
+      <BrowserRouter basename="/list-coun">
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/list-coun" element={<LoginPage />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </Provider>
   );
 }
